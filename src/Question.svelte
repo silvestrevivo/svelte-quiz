b47f7d2f0953c4bd1b0e54fcc62cff4523cb08e2
silvestrevivo <silvestrevivomillan@gmail.com>
2019-10-14 13:10:39 +0200

    quiz working

--- src/Question.svelte
+++ src/Question.svelte
@@ -1,14 +1,45 @@
 <script>
   export let question;
 
-  let answers = question.incorrect_answers;
+  let isCorrect;
+  let isAnswered = false;
+  let answers = question.incorrect_answers.map(answer => {
+    return {
+      answer,
+      correct: false
+    };
+  });
+
+  let allAnswers = [
+    ...answers,
+    { answer: question.correct_answer, correct: true }
+  ];
+
+  shuffle(allAnswers);
+
+  function shuffle(array) {
+    array.sort(() => Math.random() - 0.5);
+  }
+
+  function checkQuestion(correct) {
+    isAnswered = true;
+    isCorrect = correct;
+  }
 </script>
 
 <h3>
   {@html question.question}
 </h3>
-{#each answers as answer}
-  <button>
-    {@html answer}
+
+{#if isAnswered}
+  <h4>
+    {#if isCorrect}You got it right{:else}Try it again{/if}
+  </h4>
+{/if}
+
+{#each allAnswers as answer}
+  <button on:click={() => checkQuestion(answer.correct)}>
+    {@html answer.answer}
+    {answer.correct}
   </button>
 {/each}
